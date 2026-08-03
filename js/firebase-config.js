e43249387ce909da5c555ac5e6d68518bf62dd82
antito-yanac <kevin.25hxl@gmail.com>
2026-08-02 21:32:02 -0500

    add config

---
+++ js/firebase-config.js
@@ -0,0 +1,17 @@
+// js/firebase-config.js
+//
+// Configuración de Firebase para Web (app web registrada en Firebase Console).
+// appId web: 1:301615802026:web:d1a6cec0c1e21a71ad92d3
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyDc5EUjoE5HjkVWnTGJwWSDraszijxBz1E",
+    authDomain: "antitomessage.firebaseapp.com",
+    projectId: "antitomessage",
+    storageBucket: "antitomessage.firebasestorage.app",
+    messagingSenderId: "301615802026",
+    appId: "1:301615802026:web:d1a6cec0c1e21a71ad92d3",
+    measurementId: "G-2NWGFM3L53"
+};
+
+// Par de claves VAPID para FCM Web Push (generado en Firebase Console)
+export const vapidKey = "BL5zwoCQ_l-Z0b_HFo2gLW527S0pRrXu1S-u0Tpl8yPVtsPQfv89GH5snIjUUMz4y3YwRg-kf6hKlZIrqjbsEwQ";
